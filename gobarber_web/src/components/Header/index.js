fd43c04881709ee73c45dffad06e23a04ca33359
devmeist3r <devmeist3r@gmail.com>
2019-08-11 00:38:04 -0300

    -

--- gobarber_web/src/components/Header/index.js
+++ gobarber_web/src/components/Header/index.js
@@ -2,11 +2,11 @@ import React from 'react'
 
 import { Link } from 'react-router-dom'
 
+import Notifications from '~/components/Notifications'
+
 import logo from '~/assets/logoRoxo.svg'
 
 import { Container, Content, Profile } from './styles'
-
-// import { Container } from './styles';
 
 export default function Header() {
   return (
@@ -18,6 +18,8 @@ export default function Header() {
         </nav>
 
         <aside>
+          <Notifications />
+
           <Profile>
             <div>
               <strong>Lucas Inocencio</strong>
