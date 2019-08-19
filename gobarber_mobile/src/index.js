4ba811412417b59b6accab8b507cda0bedc94f39
p21lucasinocencio <lucas.inocencio@p21sistemas.com.br>
2019-08-19 13:03:17 -0300

    -

--- gobarber_mobile/src/index.js
+++ gobarber_mobile/src/index.js
@@ -1,9 +1,21 @@
 import React from 'react'
+import { PersistGate } from 'redux-persist/integration/react'
+import { Provider } from 'react-redux'
 
-// import { Container } from './styles';
+import { StatusBar } from 'react-native'
 
 import Routes from './routes'
+import '~/config/ReactotronConfig'
+
+import { store, persistor } from './store'
 
 export default function App() {
-  return <Routes />
+  return (
+    <Provider store={store}>
+      <PersistGate persistor={persistor}>
+        <StatusBar barStyle="light-content" backgroundColor="#7159c1" />
+        <Routes />
+      </PersistGate>
+    </Provider>
+  )
 }
--- gobarber_mobile/src/pages/SignIn/index.js
+++ gobarber_mobile/src/pages/SignIn/index.js
@@ -1,25 +1,69 @@
-import React from 'react'
+import React, { useRef } from 'react'
 
+import PropTypes from 'prop-types'
+
+import { Image } from 'react-native'
+
+import DismissKeyboard from '~/components/DismissKeyboard'
 import Background from '~/components/Background'
 
-import Input from '~/components/Input'
-import Button from '~/components/Button'
+import logo from '~/assets/logo.png'
 
-// import { Container } from './styles';
+import {
+  Container,
+  Form,
+  FormInput,
+  SubmitButton,
+  SignLink,
+  SignLinkText,
+} from './styles'
 
-export default function SignIn() {
+export default function SignIn({ navigation }) {
+  const passwordRef = useRef()
+
+  function handleSubimit() {}
+
   return (
-    <Background>
-      <Input
-        style={{ marginTop: 30 }}
-        icon="call"
-        placeholder="Digite seu nome"
-      />
-      <Button>Entrar</Button>
-    </Background>
+    <DismissKeyboard>
+      <Background>
+        <Container>
+          <Image source={logo} />
+          <Form>
+            <FormInput
+              icon="mail-outline"
+              keyboardType="email-address"
+              autoCorrect={false}
+              autoCapitalize="none"
+              placeholder="Digite seu e-mail"
+              returnKeyType="next"
+              onSubmitEditing={() => passwordRef.current.focus()}
+            />
+            <FormInput
+              icon="lock-outline"
+              autoCorrect={false}
+              secureTextEntry
+              placeholder="Sua senha secreta"
+              ref={passwordRef}
+              returnKeyType="send"
+              onSubmitEditing={() => handleSubimit()}
+            />
+            <SubmitButton onPress={handleSubimit}>Acessar</SubmitButton>
+          </Form>
+          <SignLink
+            onPress={() => {
+              navigation.navigate('SignUp')
+            }}
+          >
+            <SignLinkText>Criar uma conta</SignLinkText>
+          </SignLink>
+        </Container>
+      </Background>
+    </DismissKeyboard>
   )
 }
 
-SignIn.navigationOptions = {
-  title: 'Login',
+SignIn.propTypes = {
+  navigation: PropTypes.shape({
+    navigate: PropTypes.func,
+  }).isRequired,
 }
