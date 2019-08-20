99835af3863f21cbf8c5f98869176c7bc47a933e
devmeist3r <devmeist3r@gmail.com>
2019-08-19 22:37:50 -0300

    -

--- gobarber_mobile/src/routes.js
+++ gobarber_mobile/src/routes.js
@@ -1,15 +1,30 @@
-import {
-  createAppContainer,
-  createSwitchNavigator,
-  createBottomTabNavigator,
-} from 'react-navigation'
+import React from 'react'
+
+import { createAppContainer, createBottomTabNavigator } from 'react-navigation'
+
+import createAnimatedSwitchNavigator from 'react-navigation-animated-switch'
+import { Transition } from 'react-native-reanimated'
 
 import SignIn from '~/pages/SignIn'
 import SignUp from '~/pages/SignUp'
 
 export default createAppContainer(
-  createSwitchNavigator({
-    SignIn,
-    SignUp,
-  })
+  createAnimatedSwitchNavigator(
+    {
+      SignIn,
+      SignUp,
+    },
+    {
+      transition: (
+        <Transition.Together>
+          <Transition.Out
+            type="slide-bottom"
+            durationMs={400}
+            interpolation="easeIn"
+          />
+          <Transition.In type="fade" durationMs={500} />
+        </Transition.Together>
+      ),
+    }
+  )
 )
